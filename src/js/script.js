123371f8c3457ac4deb08c5f671b6a8979916351
Stas Remnev <remnev.dev@gmail.com>
2024-03-23 12:16:47 +0300

    feat widget: info

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,1 @@
-const sliderInput = document.querySelector(".slider-input");
-
-function updateSliderRangeBackground() {
-  const percent =
-    ((sliderInput.value - sliderInput.min) /
-      (sliderInput.max - sliderInput.min)) *
-    100;
-  sliderInput.style.background = `linear-gradient(to right, #00d8d8 ${percent}%, #f5f5f7 ${percent}%)`;
-}
-
-sliderInput.addEventListener("input", updateSliderRangeBackground);
-updateSliderRangeBackground();
+const sliderInput=document.querySelector(".slider-input");function updateSliderRangeBackground(){const e=(sliderInput.value-sliderInput.min)/(sliderInput.max-sliderInput.min)*100;sliderInput.style.background=`linear-gradient(to right, #00d8d8 ${e}%, #f5f5f7 ${e}%)`}sliderInput.addEventListener("input",updateSliderRangeBackground),updateSliderRangeBackground();
